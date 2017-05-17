7250eb417d6455a1ad6c93b18e02681769ac7b66
Matteo Crippa <matteo@boostco.de>
2017-05-17 16:10:26 +0200

    New admin script

--- script/activateAdmin.js
+++ script/activateAdmin.js
@@ -2,24 +2,24 @@ var mongoose = require('mongoose');
 var account = require('../models/account');
 var tori = require('../conf/tori.conf.js');
 
-mongoose.connect('mongodb://'+tori.conf.db.host+'/'+tori.conf.db.user);
+mongoose.connect('mongodb://' + tori.conf.db.host + '/' + tori.conf.db.user);
 
 var query = {
   username: 'a@a.it'
 };
 
-account.findOne(query, function(err, user){
-  if(err){
+account.findOne(query, function(err, user) {
+  if (err) {
     console.log(err);
   }
 
-  user.isDev = true;
-  user.save(function(err){
-    if(err){
+  user.isAdmin = true;
+  user.save(function(err) {
+    if (err) {
       console.log(err);
     }
 
-    console.log('user set as dev');
+    console.log('user set as admin');
     process.exit();
   });
 });
