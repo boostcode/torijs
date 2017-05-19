b02abd192f7126b36b6833d3975acc44963e14d6
Matteo Crippa <matteo@boostco.de>
2017-05-19 11:08:19 +0200

    Permission improved with promise

--- routes/api/permission.js
+++ routes/api/permission.js
@@ -2,23 +2,23 @@ var express = require('express');
 var router = express.Router();
 var rbac = require('mongoose-rbac');
 var permission = rbac.Permission;
+mongoose.Promise = require('bluebird');
 var _ = require('underscore');
 var error = require('../error');
 
 /// List
 router.get('/', function(req, res) {
   // FIXME: verify if only admin has to see this
-  permission.find({}, function(err, perms) {
-    if (err) {
+  permission.find({}).exec()
+    .then(function(perms) {
+      res.json({
+        success: true,
+        permission: _.groupBy(perms, 'subject');
+      });
+    })
+    .catch(function(err) {
       return error(res, 500, err.message);
-    }
-
-    res.json({
-      success: true,
-      permission: _.groupBy(perms, 'subject')
     });
-
-  });
 });
 
 module.exports = router;
